d37dc12973429ec7fc8cdadbfd2378da8d9d4530
tymosh <77159957+tymoshh@users.noreply.github.com>
2024-07-25 22:33:34 +0100

    Delete download.sh

--- download.sh
+++
@@ -1,4 +0,0 @@
-curl https://raw.githubusercontent.com/tymoshh/tempRepo/main/main.py -o main.py
-curl https://raw.githubusercontent.com/tymoshh/tempRepo/main/getPrice.py -o getPrice.py
-curl https://raw.githubusercontent.com/tymoshh/tempRepo/main/webhookSender.py -o webhookSender.py
-curl https://raw.githubusercontent.com/tymoshh/tempRepo/main/config.txt -o config.txt
