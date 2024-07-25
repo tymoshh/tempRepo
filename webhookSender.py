f3f3d44a0d35e02140e92c4432cb58329428d6fc
tymosh <77159957+tymoshh@users.noreply.github.com>
2024-07-25 22:33:53 +0100

    Delete webhookSender.py

--- webhookSender.py
+++
@@ -1,8 +0,0 @@
-import requests
-from discord import SyncWebhook
-
-WEBHOOK_URL = ""
-
-def sendMsg(stringMessage, WEBHOOK_URL):
-    MY_WEBHOOK = SyncWebhook.from_url(WEBHOOK_URL)
-    MY_WEBHOOK.send(content=stringMessage)
